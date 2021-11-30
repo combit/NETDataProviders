ee94781fc14dc133dd4db552c6c7c2e0b2c3606e
Erdal Alacali <alacali@combit.net>
2021-11-30 15:27:46 +0100

    Release 27.000

---
+++ TasteITConsulting.ListLabel.OpenEdgeDataProvider/.NET 5/OpenEdgeDemoRest/DemoForm.cs
@@ -0,0 +1,127 @@
+﻿using combit.Reporting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TasteITConsulting.Reporting;
+
+namespace OpenEdgeDemo
+{
+    public partial class DemoForm : Form
+    {
+        OpenEdgeConfiguration Config = null;
+
+        public DemoForm()
+        {
+            InitializeComponent();
+            Config = new OpenEdgeConfiguration();
+            Config.loadSettings();
+        }
+
+        private void LLAction (string action)
+        {
+            var param = new RestConnectionParameter();
+            param.RestURL  = Properties.Settings.Default.RestURL;
+            param.Userid   = Properties.Settings.Default.Userid;
+            param.Password = Properties.Settings.Default.Password;
+
+            var ll = new ListLabel();
+            var dp = new OpenEdgeDataProvider();
+            dp.ServiceAdapter = new RestServiceAdapter(param);
+            dp.ServiceName = Properties.Settings.Default.ServiceName;
+            dp.Initialize();
+            ll.DataSource = dp;
+            try
+            {
+                if (action == "DESIGN")
+                {
+                    if (Properties.Settings.Default.DesignerPreviewMaxRows > 0)
+                    {
+                        dp.MaxRows = Properties.Settings.Default.DesignerPreviewMaxRows;
+                    }
+                    ll.Design();
+                }
+                else if (action == "PRINT")
+                {
+                    ll.Print();
+                }
+                else
+                {
+
+                }
+            }
+            catch (ListLabelException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            ll.Dispose();
+            
+            dp.Dispose();
+
+        }
+
+        private void buttonDesign_Click(object sender, EventArgs e)
+        {
+            LLAction("DESIGN");
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            LLAction("PRINT");
+            
+        }
+
+        private void buttonSettings_Click(object sender, EventArgs e)
+        {
+            SettingsForm f = new SettingsForm();
+            f.Settings = Config;
+            if (f.ShowDialog(this) == DialogResult.OK)
+            {
+                Config.saveSettings();
+                /*
+                if (OpenEdgeServiceAdapter != null)
+                {
+                    OpenEdgeServiceAdapter.Disconnect();
+                }
+                */
+            }
+            f.Dispose();
+        }
+    }
+
+    public class OpenEdgeConfiguration
+    {
+        public string RestURL { get; set; }
+        public string Userid { get; set; }
+        public string Password { get; set; }
+        public string ServiceName { get; set; }
+        public int DesignerPreviewMaxRows { get; set; }
+
+        public void loadSettings()
+        {
+            this.ServiceName = Properties.Settings.Default.ServiceName;
+            this.RestURL = Properties.Settings.Default.RestURL;
+            this.Userid = Properties.Settings.Default.Userid;
+            this.Password = Properties.Settings.Default.Password;
+            this.DesignerPreviewMaxRows = Properties.Settings.Default.DesignerPreviewMaxRows;
+        }
+
+        public void saveSettings()
+        {
+            Properties.Settings.Default.ServiceName = this.ServiceName;
+            Properties.Settings.Default.RestURL = this.RestURL;
+            Properties.Settings.Default.Userid = this.Userid;
+            Properties.Settings.Default.Password = this.Password;
+            Properties.Settings.Default.DesignerPreviewMaxRows = this.DesignerPreviewMaxRows;
+            Properties.Settings.Default.Save();
+        }
+
+    }
+
+
+}
--- TasteITConsulting.ListLabel.OpenEdgeDataProvider/.NET 5/OpenEdgeDemoRest/SettingsForm.Designer.cs
+++ TasteITConsulting.ListLabel.OpenEdgeDataProvider/.NET 5/OpenEdgeDemoRest/SettingsForm.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace OpenEdgeDemo
+﻿
+namespace OpenEdgeDemo
 {
     partial class SettingsForm
     {
@@ -38,19 +39,21 @@
             this.SettingsGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.SettingsGrid.Location = new System.Drawing.Point(-1, 2);
+            this.SettingsGrid.HelpVisible = false;
+            this.SettingsGrid.Location = new System.Drawing.Point(12, 12);
             this.SettingsGrid.Name = "SettingsGrid";
-            this.SettingsGrid.Size = new System.Drawing.Size(509, 312);
+            this.SettingsGrid.PropertySort = System.Windows.Forms.PropertySort.NoSort;
+            this.SettingsGrid.Size = new System.Drawing.Size(550, 316);
             this.SettingsGrid.TabIndex = 0;
-            this.SettingsGrid.Click += new System.EventHandler(this.SettingsGrid_Click);
+            this.SettingsGrid.ToolbarVisible = false;
             // 
             // buttonOK
             // 
             this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonOK.Location = new System.Drawing.Point(12, 327);
+            this.buttonOK.Location = new System.Drawing.Point(12, 334);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
-            this.buttonOK.TabIndex = 1;
+            this.buttonOK.TabIndex = 3;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
             this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
@@ -58,31 +61,24 @@
             // buttonCancel
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(107, 327);
+            this.buttonCancel.Location = new System.Drawing.Point(93, 334);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
-            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.TabIndex = 4;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
-            this.AcceptButton = this.buttonOK;
-            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(509, 362);
+            this.ClientSize = new System.Drawing.Size(574, 369);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.SettingsGrid);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "SettingsForm";
-            this.ShowInTaskbar = false;
             this.Text = "Settings";
-            this.Load += new System.EventHandler(this.Settings_Load);
+            this.Load += new System.EventHandler(this.SettingsForm_Load);
             this.ResumeLayout(false);
 
         }
