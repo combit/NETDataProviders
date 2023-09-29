87fed946861e69e5c23a0b9a41a549312d160f3e
Daniel Kenner <kenner@combit.net>
2023-09-29 15:21:33 +0200

    Added data providers for Cosmos DB and MySQL

---
+++ combit.ListLabel.CosmosDBDataProvider/cosmosDBDataProvider.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+
+namespace combit.Reporting.DataProviders
+{
+    public sealed class CosmosDbDataProvider<T> : IDataProvider, ISupportsLogger
+
+    {
+        private ILlLogger _logger;
+        private Database _databaseID;
+
+        private string _sqlQueryText = string.Empty;
+
+        private ObjectDataProvider _objectDataProvider;
+
+        private Container _containerID;
+
+        public bool SupportsAnyBaseTable => ((IDataProvider)_objectDataProvider).SupportsAnyBaseTable;
+
+        public ReadOnlyCollection<ITable> Tables => ((IDataProvider)_objectDataProvider).Tables;
+
+        public ReadOnlyCollection<ITableRelation> Relations => ((IDataProvider)_objectDataProvider).Relations;
+
+        public CosmosDbDataProvider(string uri, string token, string database, string container, string sqlQuery = null)
+
+        {
+            try
+            {
+                CosmosClient cosmosClient = new CosmosClient(uri, token, new CosmosClientOptions() { ApplicationName = null });
+                _databaseID = cosmosClient.GetDatabase(database);
+                _containerID = _databaseID.GetContainer(container);
+                _sqlQueryText = sqlQuery;
+                _objectDataProvider = QueryItems(_sqlQueryText);
+            }
+            catch (CosmosException ex)
+            {
+                Logger.Error(LogCategory.DataProvider, "Error while querying cosmosDB: {0}.", ex.Message);
+                throw;
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Logger.Error(LogCategory.DataProvider, "Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+        }
+        internal ILlLogger Logger { get { return _logger ?? LoggingHelper.DummyLogger; } }
+        public void SetLogger(ILlLogger logger, bool overrideExisting)
+        {
+            if (_logger == null || overrideExisting)
+            {
+                _logger = logger;
+            }
+        }
+       
+        public ObjectDataProvider QueryItems(string sqlQuery)
+        {
+            string sqlQueryText = String.Empty;
+            sqlQueryText = string.IsNullOrEmpty(sqlQuery) ? "SELECT * FROM c" : sqlQuery;
+            
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+
+            FeedIterator<T> queryResultSetIterator = _containerID.GetItemQueryIterator<T>(queryDefinition);
+
+            var task = queryResultSetIterator.ReadNextAsync();
+            var results = task.Result;
+
+            ObjectDataProvider objectDataProvider = new ObjectDataProvider(results);
+            return objectDataProvider;
+
+        }
+
+        public ITable GetTable(string tableName)
+        {
+            return ((IDataProvider)_objectDataProvider).GetTable(tableName);
+        }
+
+        public ITableRelation GetRelation(string relationName)
+        {
+            return ((IDataProvider)_objectDataProvider).GetRelation(relationName);
+        }
+    }
+}
---
+++ combit.ListLabel.MySqlConnectionDataProvider/MySqlConnectionDataProvider.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using MySqlConnector;
+
+namespace combit.Reporting.DataProviders
+{
+    /// <summary>
+    /// Provider for MySql, see https://mysqlconnector.net/
+    /// </summary>
+    [Serializable]
+    public class MySqlConnectionDataProvider : DbConnectionDataProvider, ISerializable
+    {
+        protected MySqlConnectionDataProvider()
+        {
+            SupportedElementTypes = DbConnectionElementTypes.Table | DbConnectionElementTypes.View;
+            Provider.CancelBeforeClose = false;
+            SupportsAdvancedFiltering = true;
+        }
+
+        protected MySqlConnectionDataProvider(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            int version = info.GetInt32("MySqlConnectionDataProvider.Version");
+            if (version >= 1)
+            {
+                Connection = CreateDbConnection(info.GetString("ConnectionString"));
+                Connection.ConnectionString = info.GetString("ConnectionString");
+                SupportedElementTypes = (DbConnectionElementTypes)info.GetInt32("SupportedElementTypes");
+                Provider.CancelBeforeClose = false;
+            }
+            if (version >= 2)
+            {
+                SupportsAdvancedFiltering = info.GetBoolean("SupportsAdvancedFiltering");
+            }
+        }
+
+        public MySqlConnectionDataProvider(IDbConnection connection)
+            :this()
+        {
+            if (!(connection is MySqlConnection))
+                throw new ListLabelException("The connection object must be a valid MySQL connection");
+            Connection = connection;
+        }
+
+
+        public MySqlConnectionDataProvider(string connectionString)
+            :this()
+        {
+            Connection = CreateDbConnection(connectionString);
+        }
+
+        public DbConnectionElementTypes SupportedElementTypes { get; set; }
+        
+        public override bool SupportsAdvancedFiltering { get; set; }
+
+        private static DbConnection CreateDbConnection(string connectionString)
+        {
+            // Assume failure.
+            DbConnection connection = null;
+
+            // Create the DbConnection.
+            if (connectionString != null)
+            {
+                try
+                {
+                    connection = new MySqlConnection(connectionString);                    
+                }
+                catch (Exception ex)
+                {
+                    // Set the connection to null if it was created.
+                    if (connection != null)
+                    {
+                        connection = null;
+                    }
+                    LoggingHelper.LogExceptionDetails(ex);
+                    throw;
+                }
+            }
+            // Return the connection.
+            return connection;
+        }
+
+        protected override void Init()
+        {
+            if (Initialized)
+                return;
+
+            HashSet<String> passedRelationNames = new HashSet<string>();
+            HashSet<String> passedSchemas = new HashSet<string>();
+            HashSet<String> passedTables = new HashSet<string>();
+            HashSet<String> excludedOwners = new HashSet<string>();
+            excludedOwners.Add("mysql");
+            excludedOwners.Add("information_schema");
+            excludedOwners.Add("sys");
+
+            if (Connection.State != ConnectionState.Open)
+                Connection.Open();
+
+            DbCommand cmd;
+            try
+            {
+                DataTable views = null;
+                DataTable tables = null;
+                if ((SupportedElementTypes & DbConnectionElementTypes.Table) != 0)
+                {
+                    tables = (Connection as DbConnection).GetSchema("Tables");
+                    DataProviderHelper.LogDataTableStructure(Logger, tables);
+                }
+                if ((SupportedElementTypes & DbConnectionElementTypes.View) != 0)
+                {
+                    views = (Connection as DbConnection).GetSchema("Views");
+                    DataProviderHelper.LogDataTableStructure(Logger, views);
+                }
+                Connection.Close();
+
+                for (int pass = 0; pass < 2; pass++)
+                {
+                    DataTable currentTable;
+                    switch (pass)
+                    {
+                        case 0:
+                            currentTable = views;
+                            break;
+                        case 1:
+                            currentTable = tables;
+                            break;
+                        default:
+                            currentTable = null;
+                            break;
+                    }
+
+                    if (currentTable == null)
+                        continue;
+
+                    foreach (DataRow dr in currentTable.Rows)
+                    {
+                        string schema = dr["TABLE_SCHEMA"].ToString();
+                        string name = dr["TABLE_NAME"].ToString();
+
+                        if (SuppressAddTableOrRelation(name, schema))
+                            continue;
+
+                        if (excludedOwners.Contains(schema))
+                            continue;
+
+                        // Get db from connection string
+                        DbConnection connection = (DbConnection)Connection;
+                        string database = connection.Database;
+
+                        // If no database is specified in the connectionString, get all tables of all schemas,
+                        // otherwise just get the tables of the specified db
+                        if ((schema != database) && !String.IsNullOrEmpty(database))
+                            continue;
+
+                        if (pass == 1)
+                        {
+                            // check if this is a view and the views table contains the current table
+                            // see #61502, sometimes a view is contained in both schema tables
+                            if (dr["TABLE_TYPE"].ToString() == "VIEW")
+                            {
+                                if (passedTables.Contains(name))
+                                    continue;
+                            }
+                        }
+
+                        // No schema specified, add them to list, to build relations
+                        if (!passedSchemas.Contains(schema))
+                        {
+                            passedSchemas.Add(schema);
+                        }
+
+                        ICloneable cloneable = (ICloneable)Connection;
+                        DbConnection newConnection = (DbConnection)cloneable.Clone();
+                        cmd = newConnection.CreateCommand();
+                        cmd.CommandText = "Select * From " + (String.IsNullOrEmpty(schema) ? "`" + name + "`" : "`" + schema + "`.`" + name + "`") + "";
+                        AddCommand(cmd, name, "`{0}`", "?{0}");
+                        passedTables.Add(name);
+                    }
+                }
+
+                foreach (string schema in passedSchemas)
+                {
+                    string commandText = String.Format(CultureInfo.InvariantCulture, "SELECT K.`TABLE_NAME`, K.`COLUMN_NAME`, K.`REFERENCED_TABLE_NAME`, K.`REFERENCED_COLUMN_NAME`, K.`CONSTRAINT_NAME` FROM information_schema.KEY_COLUMN_USAGE K WHERE `TABLE_SCHEMA`='{0}' ORDER BY K.`CONSTRAINT_NAME`, K.`POSITION_IN_UNIQUE_CONSTRAINT`", schema);
+                    cmd = (Connection as DbConnection).CreateCommand();
+                    cmd.CommandText = commandText;
+                    using (cmd)
+                    {
+                        Connection.Open();
+                        DbDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                        string lastConstraintName = string.Empty;
+
+                        while (reader.Read())
+                        {
+                            if (!reader.IsDBNull(2) && !reader.IsDBNull(3))
+                            {
+                                string childColumnName = reader.GetString(1);
+                                string parentColumnName = reader.GetString(3);
+                                string childTableName = reader.GetString(0);
+                                string parentTableName = reader.GetString(2);
+                                string constraintName = reader.GetString(4);
+                                if (SuppressAddTableOrRelation(parentTableName, null) || SuppressAddTableOrRelation(childTableName, null))
+                                    continue;
+
+                                if (lastConstraintName == constraintName)
+                                {
+                                    IDataProvider providerInterface = (Provider as IDataProvider);
+                                    DbCommandTableRelation lastRelation = providerInterface.Relations[providerInterface.Relations.Count - 1] as DbCommandTableRelation;
+                                    lastRelation.ChildColumnName += '\t' + childColumnName;
+                                    lastRelation.ParentColumnName += '\t' + parentColumnName;
+                                    continue;
+                                }
+                                lastConstraintName = constraintName;
+
+                                string relName = parentTableName + "2" + childTableName;
+                                int relationIndex = 1;
+                                string formatString = relName + "{0}";
+
+                                while (passedRelationNames.Contains(relName))
+                                {
+                                    relName = String.Format(CultureInfo.InvariantCulture, formatString, relationIndex);
+                                    relationIndex++;
+                                }
+                                passedRelationNames.Add(relName);
+                                AddRelation(relName, parentTableName, childTableName, parentColumnName, childColumnName);
+                            }
+                        }
+                        reader.Close();
+                    }
+                }
+            }
+            finally
+            {
+                Connection.Close();
+                Initialized = true;
+            }
+        }
+        protected override void OnTranslateFilterSyntax(object sender, TranslateFilterSyntaxEventArgs e)
+        {
+            base.OnTranslateFilterSyntax(sender, e);
+            if (e.Handled)
+                return;
+
+            switch (e.Part)
+            {
+                case LlExpressionPart.BinaryOperatorAdd:
+                    {
+                        if (e.Arguments[0] is String && e.Arguments[1] is String)
+                        {
+                            e.Result = String.Format("CONCAT({0},{1})", e.Arguments[0], e.Arguments[1]);
+                            e.Handled = true;
+                        }
+                    }
+                    break;
+                case LlExpressionPart.Function:
+                    {
+                        switch (e.Name.ToString().ToUpper())
+                        {
+                            case "STARTSWITH":
+                                e.Result = String.Format("({0} LIKE CONCAT({1},'%'))", e.Arguments[0], e.Arguments[1]);
+                                e.Handled = true;
+                                break;
+                            case "ENDSWITH":
+                                e.Result = String.Format("({0} LIKE CONCAT('%',{1}))", e.Arguments[0], e.Arguments[1]);
+                                e.Handled = true;
+                                break;
+                            case "CONTAINS":
+                                e.Result = String.Format("({0} LIKE CONCAT('%', CONCAT({1},'%')))", e.Arguments[0], e.Arguments[1]);
+                                e.Handled = true;
+                                break;
+                            case "LEN":
+                                e.Result = String.Format("(LENGTH({0}))", e.Arguments[0]);
+                                e.Handled = true;
+                                break;
+                            case "EMPTY":
+                                if (e.ArgumentCount == 1)
+                                    e.Result = String.Format("(LENGTH({0}) = 0)", e.Arguments[0]);
+                                else
+                                    if ((bool)e.Arguments[1])
+                                        e.Result = String.Format("(LENGTH(LTRIM(RTRIM({0}))) = 0)", e.Arguments[0]);
+                                    else
+                                        e.Result = String.Format("(LENGTH({0}) = 0)", e.Arguments[0]);
+                                e.Handled = true;
+                                break;
+
+                            default:
+                                break;
+                        }
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        //http://dev.mysql.com/doc/refman/5.0/en/group-by-functions.html
+        protected override string GetNativeAggregateFunctionName(NativeAggregateFunction function)
+        {
+            switch (function)
+            {
+                case NativeAggregateFunction.StdDevSamp:
+                    return "STDDEV_SAMP";
+                case NativeAggregateFunction.StdDevPop:
+                    return "STDDEV";
+                case NativeAggregateFunction.VarSamp:
+                    return "VARIANCE_SAMP";
+                case NativeAggregateFunction.VarPop:
+                    return "VARIANCE";
+                default:
+                    return function.ToString().ToUpperInvariant();
+            }
+        }
+
+        #region ISerializable Members
+
+#if !NET_BUILD
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+#endif
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("MySqlConnectionDataProvider.Version", 2);
+            info.AddValue("ConnectionString", Connection.ConnectionString);
+            info.AddValue("SupportedElementTypes", (int)SupportedElementTypes);
+            info.AddValue("SupportsAdvancedFiltering", SupportsAdvancedFiltering);
+        }
+
+#endregion
+    }
+    [Serializable]
+    public class MariaDBConnectionDataProvider : MySqlConnectionDataProvider
+    {
+        protected MariaDBConnectionDataProvider()
+            : base()
+        {
+        }
+
+        protected MariaDBConnectionDataProvider(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public MariaDBConnectionDataProvider(IDbConnection connection)
+            : base(connection)
+        {
+        }
+
+        public MariaDBConnectionDataProvider(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+}
+
